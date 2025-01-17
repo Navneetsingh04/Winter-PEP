caece38aa436231e7cdddf9ec025c6fb0fe88490
Navneet Singh <navneetsingh1825@gmail.com>
2025-01-17 18:35:20 +0530

    Initial commit

---
+++ Day 4/arrowFunc.js
@@ -0,0 +1,24 @@
+// const sum = (a,b) => a+b;
+// console.log(sum(76,67));
+
+
+// const test = (x,y,z) =>{
+//     console.log(x+y+z);
+// }
+// test(2,5,4);
+
+
+// const person = (firstName,lastName) =>({
+//     first: firstName,
+//     last: lastName,
+// })
+// console.log(person("Pritesh","Kumar"));
+
+
+var msg = function(){
+    console.log("Hello");
+}
+
+var person = {name1,color,age,msg};
+console.log(person.msg());
+// console.group(person.name1);
