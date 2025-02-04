4b15de510656e93b4ec0f93e962616e1275c0ce8
Navneet Singh <navneetsingh1825@gmail.com>
2025-02-04 11:47:24 +0530

    bootstrap

---
+++ react-bootstrap/src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
